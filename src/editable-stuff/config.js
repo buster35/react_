76854ec4fabe8260867f48f3905d2a8492980044
Zach <121834256+buster35@users.noreply.github.com>
2023-08-15 21:06:26 -0500

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -1,5 +1,4 @@
 import profilePic from "../assets/profile-pic.jpg";
-const resume = require("../editable-stuff/zg-resume.pdf");
 
 // Navigation Bar SECTION
 const navBar = {
@@ -47,7 +46,6 @@ const about = {
   imageSize: 375,
   message:
     "I have a fundamental understanding of relevant front & back-end technologies, and I envision my programming repertoire growing as I continue my quest to learn more languages. I truly admire the wide array of computing languages and the practical uses each serve to accomplish, both niche and mainstream.",
-  resume: resume,
 };
 
 // PROJECTS SECTION
